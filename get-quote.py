c4556d1351a555064f3de13ed6d3048064d0516b
Klokan1 <philipp.schoerkhuber@gmail.com>
2021-05-04 18:48:42 +0200

    Save

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
+
+
 def primary():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[13])
 
 if __name__== "__main__":
   primary()
+ 
