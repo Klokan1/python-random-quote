41f64c2d67bf47a94cc119192d97337590e1c329
Klokan1 <philipp.schoerkhuber@gmail.com>
2021-05-04 21:35:49 +0200

    Save

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
 import random
 
-
 def primary():
-  print("Keep it logically awesome.")
+    print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    f = open("C:\\Users\Admin\OneDrive\Projekte\Atom Projekte\Python\Github\quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  print(quotes[13])
+    print(quotes[13])
 
 if __name__== "__main__":
-  primary()
+    primary()
+
+
+ 
