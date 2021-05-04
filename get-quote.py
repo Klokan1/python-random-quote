f471dbb8f0129f1360fe2ffa4cd05f8e66f317bc
Klokan1 <philipp.schoerkhuber@gmail.com>
2021-05-04 21:50:42 +0200

    Save

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,16 @@
 import random
 
 def primary():
-    print("Keep it logically awesome.")
 
     f = open("C:\\Users\Admin\OneDrive\Projekte\Atom Projekte\Python\Github\quotes.txt")
     quotes = f.readlines()
     f.close()
 
     last = len(quotes) - 1
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    rnd = random.sample(range(0, last), 2)
+    print(quotes[rnd[0]], quotes[rnd[1]])
+
+    
 
 if __name__== "__main__":
     primary()
