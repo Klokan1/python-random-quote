0dd451355202cb66036d86f6bf62906166f71a26
Klokan1 <philipp.schoerkhuber@gmail.com>
2021-05-04 21:37:52 +0200

    Final

--- get-quote.py
+++ get-quote.py
@@ -7,10 +7,9 @@ def primary():
     quotes = f.readlines()
     f.close()
 
-    print(quotes[13])
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
 
 if __name__== "__main__":
     primary()
-
-
- 
