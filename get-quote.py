555d0a097f31b4ac18a8618502770dabfeb7b836
Klokan1 <philipp.schoerkhuber@gmail.com>
2021-05-05 17:07:01 +0200

    Delete Stuff

--- get-quote.py
+++ get-quote.py
@@ -2,15 +2,26 @@ import random
 
 def primary():
 
-    f = open("C:\\Users\Admin\OneDrive\Projekte\Atom Projekte\Python\Github\quotes.txt")
-    quotes = f.readlines()
-    f.close()
+    with open("C:\\Users\Admin\OneDrive\Projekte\Atom Projekte\Python\Github\quotes.txt", "r") as f:
+        lines = f.readlines()
+        #f.write("This has been added!\n")
 
-    last = len(quotes) - 1
-    rnd = random.sample(range(0, last), 2)
-    print(quotes[rnd[0]], quotes[rnd[1]])
+    #del(lines[14])
 
-    
+
+
+    with open("C:\\Users\Admin\OneDrive\Projekte\Atom Projekte\Python\Github\quotes.txt", "w") as f:
+        for line in lines:
+            if line.strip("\n") != "This has been added!":
+                f.write(line)
+
+    #quotes = f.readlines()
+    #last = len(quotes) - 1
+    #rnd = random.sample(range(0, last), 2)
+    #print(quotes[rnd[0]], quotes[rnd[1]])
+    print(lines)
+
+
 
 if __name__== "__main__":
     primary()
